bb20c8cc404840855e593cfd899c1e9f1524b546
Muhammad Siddiq <msiddiqshaikh10@gmail.com>
2024-12-23 09:50:59 +0500

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,16 @@ import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import connectDB from "./config";
 import bodyParser from "body-parser";
+import cors from "cors";
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.port || 3000;
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(bodyParser.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, world!");
